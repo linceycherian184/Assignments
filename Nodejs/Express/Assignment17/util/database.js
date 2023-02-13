a5453a1f910b4fe9e7f8ce09e0727c697ee1a38e
linceycherian184 <lincey184@gmail.com>
2023-02-13 22:06:56 +0530

    Assignment 17

---
+++ Nodejs/Express/Assignment17/util/database.js
@@ -0,0 +1,15 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'project1',
+//     password: 'admin123'
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize();
+
